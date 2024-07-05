24d4d755bebeffadc34a0029d920312e5c0cd52b
Eufrasio Quixindo <eufrasio.dev@gmail.com>
2024-07-05 15:22:11 +0100

    Updating Account class

--- Account.java
+++ Account.java
@@ -3,57 +3,99 @@ package com.quixindo.projects.bank;
 public class Account
 {
     public int id;
-    public String owner;
-    public double balance;
-    public String type;
-    public boolean status;
-    public double initBalance;
+    private String owner;
+    private double balance;
+    protected char type;
+    private boolean status;
+    protected double initBalance;
 
-    Account(String type, double initBalance){
-        this.initBalance = initBalance;
-        if (initBalance > 0) {
-            this.openAccount(type);
-        }
+    Account(){
+        this.setStatus(false);
+        this.setBalance(0);
     }
 
-    void openAccount(String type){
-        this.type = type;
-        this.status = true;
+    public void openAccount(char type){
+        setType(type);
+        setStatus(true);
 
-        if (this.type == "Saving") this.balance += 150.00;
-        else this.balance += 50.00;
+        if (this.type == 'c') setBalance(getBalance() + 50.00);
+        else setBalance(getBalance() + 150.00);
 
-        System.out.println("Welcome Sr. "+this.owner);
-        System.out.println(type+" account built successful");
+        System.out.println("Welcome Sr. "+getOwner());
+        System.out.println("Account built successful");
     }
 
-    void closeAccount(){
+    public void closeAccount(){
         if (this.balance > 0){
-            this.get(this.balance);
+            this.getMoney(this.balance);
             this.status = false;
+            System.out.println("Account closed successfully!!");
         }
         else System.out.println("You can not do this operation!!");
-
-        System.out.println("Building");
     }
 
-    void printType(){
-        System.out.println(this.type);
+    public void getMoney(double quantity){
+        if (getStatus() && getBalance() >= quantity) setBalance(getBalance() - quantity);
+        System.out.println("Your balance is not enough");
     }
 
-    void monthlyPay()
-    {
-        System.out.println("This account is from Standard Bank");
+    public void deposit(double quantity){
+        if (getStatus()) setBalance(getBalance() + quantity);
+        System.out.println("Your account does not exist");
     }
 
-    void get(double quantity)
-    {
-        double newBalance = this.balance - quantity;
-        this.balance = newBalance;
+    public void payMonthly(char type) {
+        double paymantV;
+        if (type == 'c')
+            paymantV = 12.00;
+        else
+            paymantV = 20.00;
+
+        if (getStatus() && getBalance() >= paymantV){
+            setBalance(getBalance() - paymantV);
+            System.out.println("Your account has been debited " + paymantV + "$ regarding monthly payment.");
+        }
+        else
+            System.out.println("Insufficience balance to pay the monthly fee.");
     }
 
-    void deposit(double quantity)
-    {
-        this.balance += quantity;
+    public String getOwner() {
+        return this.owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setType(char type) {
+        this.type = type;
+    }
+
+    public char getType() {
+        return this.type;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public boolean getStatus() {
+        return this.status;
     }
 }
--- App.java
+++ App.java
@@ -2,8 +2,6 @@ package com.quixindo.projects.bank;
 
 public class App {
     public static void main(String[] args) {
-        Account jubileu = new Account("Saving", 150.00);
-
-        jubileu.printType();
+        Account jubileu = new Account();
     }
 }
